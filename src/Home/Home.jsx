92b8e726b4fe7ec4ee4ec7deb413cccf270d736a
nikitkhakholia <khakholia.nk@gmail.com>
2022-08-07 18:19:25 +0530

    content and structure

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,7 +1,401 @@
-import React from 'react'
+import React from "react";
 
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <div>
+      <div className="row m-0 p-0">
+        <div className="col m-0 p-0">
+          <h1>
+            INTERNATIONAL CONFERENCE
+            <br />
+            ON SUSTAINABLE AND AFFORDABLE
+            <br />
+            TECHNOLOGY IN POST COVID
+          </h1>
+          <div className="row m-0 p-0">
+            <div className="col m-0 p-0">
+              <h5>Start</h5>
+              <p>09.03.2022</p>
+            </div>
+            <div className="col m-0 p-0">
+              <h5>End</h5>
+              <p>10.03.2022</p>
+            </div>
+          </div>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy
+          </p>
+        </div>
+        <div className="col m-0 p-0">
+          <div className="row m-0 p-0">
+            <div className="col m-0 p-0">
+              <p>Days</p>
+              <h5>110:</h5>
+            </div>
+            <div className="col m-0 p-0">
+              <p>Hours</p>
+              <h5>550:</h5>
+            </div>
+            <div className="col m-0 p-0">
+              <p>Minutes</p>
+              <h5>350:</h5>
+            </div>
+            <div className="col m-0 p-0">
+              <p>Seconds</p>
+              <h5>400</h5>
+            </div>
+          </div>
+
+          <div>Call for Papers</div>
+        </div>
+      </div>
+
+      <div className="row m-0 p-0">
+        <div className="col m-0 p-0">\</div>
+        <div className="col m-0 p-0">\</div>
+        <div className="col m-0 p-0">\</div>
+        <div className="col m-0 p-0">o</div>
+        <div className="col m-0 p-0">/</div>
+        <div className="col m-0 p-0">/</div>
+        <div className="col m-0 p-0">/</div>
+      </div>
+
+      {/* presentation topics */}
+      <div className="row m-0 p-0">
+        <div className="col-3 m-0 p-0">
+          <h2>Presentation Topics</h2>
+        </div>
+        <div className="col-9 m-0 p-0">
+          <div className="row m-0 p-0">
+            <div className="col m-0 p-0">
+              <h5>Artificial Topics</h5>
+              <h5>Business Intelligence</h5>
+            </div>
+            <div className="col m-0 p-0">
+              <h5>Deep Learning</h5>
+              <h5>Analytics</h5>
+            </div>
+            <div className="col m-0 p-0">
+              <h5>Neural Networks</h5>
+              <h5>IoT</h5>
+            </div>
+            <div className="col m-0 p-0">
+              <h5>Data Science</h5>
+              <h5>Cloud Computing</h5>
+            </div>
+            <div className="col m-0 p-0">
+              <h5>Computational Science</h5>
+              <h5>Cognitive Science</h5>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* important dates */}
+      <div className="row m-0 p-0">
+        <div className="col-3 m-0 p-0">
+          <h2>Important Dates</h2>
+        </div>
+        <div className="col-9 m-0 p-0">
+          <div className="row m-0 p-0">
+            <div className="col m-0 p-0">
+              <p>Last date for submission of Full Length paper</p>
+              <p>Date for Acceptance Notification</p>
+              <p>Last date for Camera Ready paper</p>
+              <p>Date for Registration</p>
+              <p>Conference Date</p>
+            </div>
+            <div className="col m-0 p-0">
+              <p>31 Jan 2021</p>
+              <p>27 Feb 2021</p>
+              <p>27 Feb 2021</p>
+              <p>28 Feb 2021</p>
+              <p>05,06 March 2021</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* keynote speakers */}
+      <div>
+        <h2>Keynote Speakers</h2>
+        <div className="row m-0 p-0">
+          <div className="col m-0 p-0">International</div>
+          <div className="col m-0 p-0">
+            <h5>Dr.Sheikh Iqbal Ahamed, Ph.D.</h5>
+            <p>
+              Director
+              <br />
+              Ubicomp Research Lab and Professor and Chair
+              <br />
+              Department of Computer Science
+              <br />
+              Marquette University, USA
+            </p>
+          </div>
+          <div className="col m-0 p-0">
+            <h5>Dr. S. S. Iyengar</h5>
+            <p>
+              ACM Fellow, IEEE Fellow, AAAS Fellow, <br />
+              Distinguished University Professor <br />
+              School of Computing <br />
+              Florida International University, USA
+            </p>
+          </div>
+          <div className="col m-0 p-0">
+            <h5>Dr Said Eid Younes</h5>
+            <p>
+              Associate Professor <br />
+              Department of Information Technology <br />
+              College of Engineering <br />
+              Univrsity of Kalamoon, SYRIA
+            </p>
+          </div>
+        </div>
+        <div className="row m-0 p-0">
+          <div className="col m-0 p-0">
+            <h5>Dr. Subhrabrata Choudhury</h5>
+            <p>
+              Professor, <br />
+              Department of Computer Science & Engineering
+              <br />
+              National Institute of Technology, Durgapur, West Bengal
+              <br />
+            </p>
+          </div>
+          <div className="col m-0 p-0">
+            <h5>Dr. Dilip Kumar Yadav</h5>
+            <p>
+              Professor,
+              <br />
+              Dept. of Computer Applications
+              <br />
+              National Institute of Technology, Jamshedpur
+            </p>
+          </div>
+          <div className="col m-0 p-0">
+            <h5>Dr. Somashekhar S. Hiremath </h5>
+            <p>
+              Associate Professor
+              <br />
+              Manufacturing Engineering Section
+              <br />
+              Department of Mechanical Engineering, IIT Madras
+            </p>
+          </div>
+          <div className="col m-0 p-0">
+            <h5>Dr. Saroj Kr. Biswas</h5>
+            <p>
+              Assistant Professor, (Grade I)
+              <br />
+              Department of Computer Science & Engineering
+              <br />
+              National Institute of Technology, Silchar
+            </p>
+          </div>
+          <div className="col m-0 p-0">National</div>
+        </div>
+      </div>
+
+      {/* call for papers */}
+      <div className="row m-0 p-0">
+        <div className="col m-0 p-0">
+          <h2>Call for Papers</h2>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy
+          </p>
+        </div>
+        <div className="col m-0 p-0">
+          <div className="row m-0 p-0">
+            <div className="col m-0 p-0">o</div>
+            <div className="col m-0 p-0"></div>
+            <div className="col m-0 p-0">/</div>
+          </div>
+          <div>Submit Your Paper</div>
+        </div>
+      </div>
+      <div className="row m-0 p-0">
+        <div className="col m-0 p-0">
+          <div className="row m-0 p-0">
+            <div className="col-4 m-0 p-0">
+              Artificial Intelligence and Robotics
+            </div>
+            <div className="col m-0 p-0">
+              Vision and non-contact sensory systems, Embedded Systems,
+              BioInspiration and Robotics: Walking And Climbing Robots, Robotic
+              Automation and Outsourcing, Drones and drone applications, New
+              Approaches in Automation and Robotics, Electrical and Electronics
+              in AI & Robotics, Robotic Surgery, Nanorobotics in Medicine,
+              Robotics Intelligence, Intelligent Control Systems and
+              Optimization, Sensors.
+            </div>
+          </div>
+          <div className="row m-0 p-0">
+            <div className="col-4 m-0 p-0">Blockchain Technology</div>
+            <div className="col m-0 p-0">
+              Blockchain in Artificial Intelligence, Machine Learning, cyber
+              security, Identity and Access Management (IAM), Blockchain tools,
+              simulators, and test-nets, Blockchain-based IoT, fog, cloud, and
+              mobile devices, Blockchain in cyber-physical systems (CPS),
+              Building private blockchain systems.
+            </div>
+          </div>
+          <div className="row m-0 p-0">
+            <div className="col-4 m-0 p-0">Cloud Computing and Visualization</div>
+            <div className="col m-0 p-0">
+              Software As A Service (SaaS), Cloud Security and Privacy, Cloud
+              Storage, Cloud Application Development, Industry-Specific Clouds,
+              Cloud Performance Management, Business Models in Cloud, 5G Cloud,
+              Serverless, Infrastructure As A Service (IaaS), Platform As A
+              Service (PaaS).
+            </div>
+          </div>
+          <div className="row m-0 p-0">
+            <div className="col-4 m-0 p-0">Cyber Security</div>
+            <div className="col m-0 p-0">
+              Cryptographic Algorithms, Risk and Reputation Management,
+              Intrusion Detection and Response, Mobile Security, Cyber security
+              Legal and Regulatory Issues, Malware Detection, Information Hiding
+              and Anonymity, Data Mining and Knowledge Discovery, Blockchain
+              Technologies and Applications, Privacy-Enhancing Models and
+              Technologies, AI/ Machine Learning for Security, Data and Software
+              Security, Database Security Privacy, Security and Trust in Social
+              Media, Identity and Trust Management, Content Protection and
+              Digital Rights Management, Authentication, Privacy and Security
+              Models, Privacy Metrics and Control, Security and Privacy in Cloud
+              and Pervasive Computing Security Architecture and Design Analysis,
+              Security Awareness and Education, Security Frameworks,
+              Architectures and Protocols Security in IoT and Edge Computing,
+              Security Testing, Software Security Assurance, Threat Awareness,
+              Trusted Computing, Virtualization Security, Vulnerability Analysis
+              and Countermeasures.
+            </div>
+          </div>
+          <div className="row m-0 p-0">
+            <div className="col-4 m-0 p-0">Data Science</div>
+            <div className="col m-0 p-0">
+              Agile Software Development and Testing, Software Testing, Big Data
+              and Data Science, Algorithms and Computational Mathematics, MDA -
+              Model Driven Architecture, MDE - Model Driven Engineering,
+              Decision Support Systems using Computer Intelligence, Deep
+              Learning and Neural Networks.
+            </div>
+          </div>
+          <div className="row m-0 p-0">
+            <div className="col-4 m-0 p-0">E-Commerce</div>
+            <div className="col m-0 p-0">
+              Machine Learning, Big Data, and Social Network Analysis, Sharing
+              Economy, Social Commerce, and Social Media, Crowdsourcing and
+              Crowdfunding Financial Technology and Markets, e-Learning and
+              Content, Impacts of FinTech on e-Commerce, e-CRM: Consumer
+              Intention and Adoption, Marketing and Pricing, e-Logistics and
+              e-Transportation, Privacy and Security, AI Approaches: Neural
+              Networks, Chatbot, and CBR, Blockchain and Cryptocurrency,
+              Globalization and Cross-Border Issues, Business Models: Innovation
+              and Entrepreneurship.
+            </div>
+          </div>
+          <div className="row m-0 p-0">
+            <div className="col-4 m-0 p-0">
+              High Performing Computer Architectures{" "}
+            </div>
+            <div className="col m-0 p-0">
+              System-on-chip and network-on-chip architectures, Multicore
+              processor architecture, Application modeling and mapping schemes
+              for multicore/SoC systems, Multicore computing and programming
+              techniques, Power-efficient architectures and techniques for
+              multicore/SoC systems, High performance software systems and its
+              applications, such as agent systems.
+            </div>
+          </div>
+          <div className="row m-0 p-0">
+            <div className="col-4 m-0 p-0">Image and Video Processing</div>
+            <div className="col m-0 p-0">
+              Action recognition, Aerial images, Anomaly detection, Automatic
+              segmentation, Background subtraction, Cognitive information
+              processing, Feature extraction and classification, Image and Video
+              Compression, Object tracking, Satellite images, Scene recognition,
+              Visual content analysis.{" "}
+            </div>
+          </div>
+          <div className="row m-0 p-0">
+            <div className="col-4 m-0 p-0">
+              Pandemic Prepardness and Digital Technology
+            </div>
+            <div className="col m-0 p-0">
+              Contact tracing: Data dashboards; migration maps; machine
+              learning; real-time data from smartphones and wearable technology
+              Screening for infection: Artificial intelligence; digital
+              thermometers; mobile phone applications; thermal cameras;
+              web-based toolkits Contact tracing: Global positioning systems;
+              mobile phone applications; real-time monitoring of mobile devices;
+              wearable technology Quarantine and self-isolation: Artificial
+              intelligence; cameras and digital recorders; global positioning
+              systems; mobile phone applications; quick response codes Clinical
+              management: Artificial intelligence for diagnostics; machine
+              learning; virtual care or telemedicine platforms Countries
+              experiences and innovations.
+            </div>
+          </div>
+          <div className="row m-0 p-0">
+            <div className="col-4 m-0 p-0">
+              Pattern Recognization and Classification
+            </div>
+
+            <div className="col m-0 p-0">
+              Probabilistic Texture Synthesis, Benchmarking, 3D Data Modelling,
+              Spatial Data Modelling, Probabilistic Expert Systems, Statistical
+              Pattern Recognition, Probabilistic Neural Networks, Content Based
+              Image Retrieval (CBIR), Dynamic Textures, Multidimensional Texture
+              Modelling Multispectral Textures, Texture Segmentation,
+              Illumination and Rotation Invariance, Medical Image Recognition,
+              Classification Mathematical Image Modelling.
+            </div>
+          </div>
+          <div className="row m-0 p-0">
+            <div className="col-4 m-0 p-0">Natural Language Processing</div>
+            <div className="col m-0 p-0">
+              Dialogue and Interactive Systems, Computational Social Science and
+              Social Media, Semantics: Lexical, Sentence level, Textual
+              Inference and Other areas, Information Retrieval and Text Mining,
+              Interpretability and Analysis of Models for NLP, NLP Applications
+              Phonology, Morphology and Word Segmentation, Language Grounding to
+              Vision, Robotics and Beyond, Machine Translation and
+              Multilinguality, Information Extraction Linguistic Theories,
+              Cognitive Modeling and Psycholinguistics.
+            </div>
+          </div>
+          <div className="row m-0 p-0">
+            <div className="col-4 m-0 p-0">Software Engineering</div>
+            <div className="col m-0 p-0">
+              Agile software development, Cloud and service-oriented computing,
+              Cooperative, distributed, and collaborative software engineering,
+              End-user software engineering, Human factors and social aspects of
+              software engineering, Mining software engineering repositories,
+              Model-driven engineering, Program comprehension and visualization,
+              Requirements engineering, Software architecture, Software
+              evolution and maintenance Software testing, Software quality,
+              Tools and environments.
+            </div>
+          </div>
+          <div className="row m-0 p-0">
+            <div className="col-4 m-0 p-0">Internet of Things (IOT)</div>
+            <div className="col m-0 p-0">
+              Smart Grid Systems, Smart Cities, IoT Architectures, Human-object
+              Interations, Interoperability of IoTs, Cyberphysical Systems.
+              WIRELESS COMMUNICATIONS : Broadcast Channels and Information
+              Theory, Cooperative Networking, Emerging Wireless Standards,
+              Heterogeneous networks (HetNets), MIMO, LTE OFDM Modulation,
+              Personal Area Networks, OFDMA, 4G Cellular, 5G Cellular, SDMA,
+              Smart Antennas, Software Defined Radios, The future of WiFi
+              Traffic Analysis for 4G, Wireless Network Control, Zigbee and
+              other Personal area networks.
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
