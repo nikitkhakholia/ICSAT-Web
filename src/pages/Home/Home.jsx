578b5b7685108dea14c1fe61cb082cda586f1f2a
MeghanaM2 <meghana.m@mca.christuniversity.in>
2022-12-05 15:11:24 +0530

    sdf

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -252,7 +252,7 @@ export default function Home() {
               </h5>
             </div>
             <div className="col m-0 p-0 text-end">
-              <h5 className="mt-4">20 February 2022</h5>
+              <h5 className="mt-4">20 February 2023</h5>
             </div>
           </div>
           <div className="row m-0 p-4 align-items-center">
